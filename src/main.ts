634f0a6e8d18cc5c3001ddb296584b5f5d617436
default-username <mihrandriatiana@gmail.com>
2024-02-19 14:58:12 +0300

    Automated commit from bot

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function main() {
     .command("enable")
     .description("Enable bot action")
     .alias("e")
-    .action(() => toggleBotAction(true))
+    .action(() =>activateBot())
   
   program
     .command('disable')
